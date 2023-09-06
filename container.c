1a818a9335658552f0ff43744ebfdff2e68fd5c5
Mino1289 <kretzvictor@orange.fr>
2023-09-06 17:12:35 +0200

    fix: Remove unnecessary prints

--- container.c
+++ container.c
@@ -1,8 +1,7 @@
 #include <container.h>
 
 void print_Container(Container c) {
-    printf("Container @%p\n", (void*) &c);
-    printf("\tID: %d\n", c.id);
+    printf("Container:\n\tID: %d\n", c.id);
     printf("\tFrom: %d\n", c.From);
     printf("\tDest: %d\n", c.Dest);
 }
